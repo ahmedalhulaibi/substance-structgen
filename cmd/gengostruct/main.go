ec85d1669b6b9fac0f54ddd1b2403db0d8c801c2
ahmedalhulaibi <ahmed.alhulaibi41@gmail.com>
2018-07-22 22:48:11 -0400

    feature: Output each obj to its own file

--- cmd/gengostruct/main.go
+++ cmd/gengostruct/main.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/ahmedalhulaibi/substance-structgen/gostruct"
 
@@ -17,18 +18,19 @@ import (
 
 func main() {
 	helpText := `Usage: 
-		gengostruct -db="dbtype" -cnstr="connection:string@locahost:9999 -file="outputFile.go"
+		gengostruct -db="dbtype" -cnstr="connection:string@locahost:9999 -file="outputFile.go" -fileperobj=false
 			or
 		gengostruct -jsonsrc="path-to-substance-objects.json -file="outputFile.go""
 			or
 		gengostruct -file="outputFile.go"	<-- Defaults to -jsonsrc=substance-objects.json
 			or
-		gengostruct							<-- Default -jsonsrc=substance-objects.json and file=gengorm.go
+		gengostruct							<-- Default -jsonsrc=substance-objects.json and creates file per obj
 `
 	dbtype := flag.String("db", "", "Database driver name.\nSupported databases types:\n\t- mysql\n\t- postgres \n\t- sqlite3\n")
 	connString := flag.String("cnstr", "", "Connection string to connect to database.")
 	jsonSourceFilePath := flag.String("jsonsrc", "substance-objects.json", "JSON substance-objects.json file describing the database objects. This can be used as an alternative to providing connection info.")
 	outputSrcFilePath := flag.String("file", "gostruct.go", "File to output source code. If blank outputs to stdout.")
+	filePerObjFlag := flag.Bool("fileperobj", true, "Creates a file for each obj/table. Default is true, if false else outputs to gostruct.go (can be overridden using 'file' option)")
 	flag.Parse()
 
 	var objects map[string]substancegen.GenObjectType
@@ -63,14 +65,30 @@ func main() {
 		objects = substancegen.GetObjectTypesFunc(*dbtype, *connString, tables)
 	}
 	if objects != nil {
-		log.Println("printing objects")
-		log.Println(objects)
-		var outputBuff bytes.Buffer
-		gostruct.GenObjectTypeToStructFunc(objects, &outputBuff)
-		err := ioutil.WriteFile(*outputSrcFilePath, outputBuff.Bytes(), 0664)
-		if err != nil {
-			fmt.Printf(helpText)
-			fmt.Printf(outputBuff.String())
+		if *filePerObjFlag {
+			for key, genobj := range objects {
+				objectsTmp := make(map[string]substancegen.GenObjectType, 0)
+				objectsTmp[key] = genobj
+				var outputBuff bytes.Buffer
+				outputBuff.WriteString("package model\n\n")
+				gostruct.GenObjectTypeToStructFunc(objectsTmp, &outputBuff)
+				err := ioutil.WriteFile(strings.Join([]string{key, ".go"}, ""), outputBuff.Bytes(), 0664)
+				if err != nil {
+					fmt.Printf(helpText)
+					fmt.Printf(outputBuff.String())
+				}
+			}
+		} else {
+			log.Println("printing objects")
+			log.Println(objects)
+			var outputBuff bytes.Buffer
+			outputBuff.WriteString("package model\n\n")
+			gostruct.GenObjectTypeToStructFunc(objects, &outputBuff)
+			err := ioutil.WriteFile(*outputSrcFilePath, outputBuff.Bytes(), 0664)
+			if err != nil {
+				fmt.Printf(helpText)
+				fmt.Printf(outputBuff.String())
+			}
 		}
 	}
 }
